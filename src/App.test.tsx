ae34c9ed77d5cec224dade4294bce2f91804236e
dsjoo725 <dsjoo725@gmail.com>
2024-05-28 10:36:44 +0900

    test: App 테스트 수정

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,8 @@ import { render } from '@testing-library/react';
 import { describe, it, expect } from 'vitest';
 import App from './App';
 
-describe('App component', () => {
-  it('올바른 제목을 렌더링해야 한다.', () => {
+describe('App 테스트', () => {
+  it('렌더링될 때 "Vite + React + Yarn Berry BoilerPlate"가 화면에 표시되어야 한다.', () => {
     const { getByText } = render(<App />);
     expect(getByText('Vite + React + Yarn Berry BoilerPlate')).toBeInTheDocument();
   });
